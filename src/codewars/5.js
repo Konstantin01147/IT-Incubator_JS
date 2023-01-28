887c795628b8493b6df49cd0650cb344e6e25a7a
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-28 23:14:09 +0300

    Commit5

--- src/codewars/5.js
+++ src/codewars/5.js
@@ -1,7 +1,20 @@
 function descendingOrder(n){
     var buf = ""+n+"";
     var arrbuf =[];
-    for(var i=0; i,buf.length;i++){
+
+    for(var i=0; i<buf.length;i++){
         arrbuf.unshift(buf.charAt(i));
     }
+    for(var i=0;i<arrbuf.length-1;i++){
+        for(var j=1;j<arrbuf.length;j++){
+            if(arrbuf[i]<arrbuf[j]){
+                var a = arrbuf[i];
+                arrbuf[i] = arrbuf[j];
+                arrbuf[j] = a;
+            }
+        }
+    }
+    buf = Number(arrbuf);
+    return buf;
+
 }
