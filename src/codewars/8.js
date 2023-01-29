516ff0a90b3d8fca771719664ddfb29f81bd59c3
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-29 21:21:52 +0300

    Commit8

--- src/codewars/8.js
+++ src/codewars/8.js
@@ -1,7 +1,7 @@
 function arrayDiff(a, b) {
-    for(var i=0;i<a.size;i++){
-        if(a[i]==b){
-            a.delete(i);
+    for (var i = 0; i < a.size; i++) {
+        if (a[i] == b) {
+            a = a.delete(i);
             i--;
         }
     }
