d30ce14a9923d817d0ceaa79ee25a49f7e501256
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-29 18:07:47 +0300

    Commit7

--- src/codewars/7.js
+++ src/codewars/7.js
@@ -1,8 +1,8 @@
 const binaryArrayToNumber = arr => {
     var output = 0;
-    for(var i=arr.size,j=0;i>0;i--,j++){
+    for(var i=0,j=0;i<arr.size;i++){
         if(arr[i]!=0){
-            output+=Math.pow(2,j);
+            output+=Math.pow(2,i-arr.size);
         }
     }
     return output;
