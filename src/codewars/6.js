a1f53ca7650995b32363eee52287c9f82c30fab0
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-29 16:43:01 +0300

    Commit6

--- src/codewars/6.js
+++ src/codewars/6.js
@@ -1,7 +1,7 @@
 function highAndLow(numbers){
     var arrNum = numbers.split(" ");
-    var max = 0;
-    var min = 0;
+    var max = arrNum[0];
+    var min = arrNum[0];
     for(var i=0;i<arrNum.length;i++){
         if(arrNum[i]<min){
             min=arrNum[i];
