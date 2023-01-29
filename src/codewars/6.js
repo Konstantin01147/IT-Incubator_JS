75c226a073b02c2f36c6a13372cf4b130a3ff3ba
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-29 16:33:27 +0300

    Commit6

--- src/codewars/6.js
+++ src/codewars/6.js
@@ -2,10 +2,10 @@ function highAndLow(numbers){
     var max = 0;
     var min = 0;
     for(var i=0;i<numbers.length;i++){
-        if(min<numbers[i]){
+        if(numbers[i]<min){
             min=numbers[i];
         }
-        if(max>numbers[i]){
+        if(numbers[i]>max){
             max=numbers[i];
         }
     }
