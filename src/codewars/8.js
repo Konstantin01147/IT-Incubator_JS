4dd14d97758b808831eee4776bebad92a2fc09fc
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-30 18:32:20 +0300

    Commit8

--- src/codewars/8.js
+++ src/codewars/8.js
@@ -1,14 +1,14 @@
 function arrayDiff(a, b) {
 
-    for (var i = 0; i < a.length; i++) {
-        for(var j=0 ; j<b.length;j++)
-        if (a[i] == b[j]) {
-            a.splice(i,1);
+    for (var j = 0; j < b.length; j++) {
+        for(var i=0 ; i<a.length;i++){
+            if (a[i] == b[j]) {
+                a.splice(i,1);
 
-            i = 0;
+                i--;
 
+            }
         }
-
     }
     console.log(a);
     return a;
