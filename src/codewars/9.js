01ef35310d02eee6aa8249330ae0fa780d42257e
Konstatnit <com.tkt.mail@yandex.ru>
2023-01-31 16:53:19 +0300

    Commit9

--- src/codewars/9.js
+++ src/codewars/9.js
@@ -1,5 +1,11 @@
 var countBits = function(n) {
     var bitsArr = Array.from(String(n),Number);
+    var numberOfOnes = 0;
     console.log(bitsArr);
-
+    for(var i=0;i<bitsArr.length;i++){
+        if(bitsArr[i]==1){
+            numberOfOnes++;
+        }
+    }
+    return numberOfOnes;
 };
